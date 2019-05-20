0a2b326915f6f20defbdb28a8ca3980befdf58f1
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2019-05-20 16:10:53 +0000

    Initial import from proof of concept

---
+++ index.mjs
@@ -0,0 +1,29 @@
+import { getFullPostFromStoredJSON, getFullPostFromObject, getShortPostFromStoredJSON, getShortPostFromObject } from './post.mjs'
+
+const basicPost = {
+  title: 'Hello world',
+  slug: 'hello-world',
+  path: '/posts/2019-05-20-hello-world/hello-world.json',
+  tags: [],
+  excerpt: 'Lorem ipsum'
+}
+
+const storedShortPost = {createdAt: '...'}
+storedShortPost.__proto__ = basicPost
+
+const storedFullPost = {body: 'Lorem ...'}
+storedFullPost.__proto__ = storedShortPost
+
+/* Main */
+let post = getShortPostFromStoredJSON(storedShortPost)
+console.log('~ getShortPostFromStoredJSON')
+console.log(post)
+
+post = getFullPostFromStoredJSON(storedFullPost)
+console.log("\n~ getFullPostFromStoredJSON")
+console.log(post)
+
+post = getShortPostFromObject(object)
+console.log("\n~ getShortPostFromObject")
+console.log(post)
+
