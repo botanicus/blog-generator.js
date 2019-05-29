3ea3b68277c83144119e83df416aa587a64002ad
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2019-05-29 06:01:27 +0000

    Test the Post constructor

--- test/parser_test.mjs
+++ test/parser_test.mjs
@@ -1,9 +1,31 @@
 import assert from 'assert'
+import Post from '../parser.mjs';
 
-describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', () => {
-      assert.equal([1, 2, 3].indexOf(4), -1)
+const markdownWithHeader = `
+# Hello world
+
+*Lorem ipsum.*
+
+## First title
+`
+
+describe('Post', () => {
+  describe('constructor', () => {
+    it('cannot be initialized without slug', () => {
+      assert.throws(() => new Post(), /slug is required/)
+    })
+
+    it('cannot be initialized without markdownWithHeader', () => {
+      assert.throws(() => new Post('hello-world'), /markdownWithHeader is required/)
+    })
+
+    it('can be initialized with slug and markdownWithHeader', () => {
+      const createPost = () => new Post('hello-world', markdownWithHeader)
+      assert.doesNotThrow(createPost)
+
+      const post = createPost()
+      assert(post.slug)
+      assert(post.markdownWithHeader)
     })
   })
 })
