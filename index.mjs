4af6e99a7df90afb36cbde612ab2afe9dd5a3e28
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2019-05-30 17:08:16 +0000

    cleanUpDirectory()

--- index.mjs
+++ index.mjs
@@ -1,8 +1,20 @@
-import { formatLongPostObject, formatShortPostObject} from './format-data-output.mjs'
-import { FileSystemActions, MoveFileAction, FileWriteAction, CreateDirectoryAction } from 'fs-actions'
+import {
+  formatLongPostObject,
+  formatShortPostObject
+} from './format-data-output.mjs'
+
+import {
+  FileSystemActions,
+  MoveFileAction,
+  FileWriteAction,
+  CreateDirectoryAction,
+  RemoveDirectoryAction,
+  RemoveFileAction
+} from 'fs-actions'
+
 import Post from './post.mjs'
 
-export function validate (content_directory) {
+export function validate (contentDirectory) {
   return true
 }
 
@@ -13,10 +25,31 @@ export function validate (content_directory) {
      a) Create output/2019-05-20-hello-world/.
      b) Generate hello-world.json from hello-world.md.
      c) Copy all the images.
-     d) Rewrite the header: add publishedAt.
+     d) Rewrite the header: add date.
   4. Generate output/posts.json and output/tags/*.json.
 */
-export function generate (content_directory, output_directory) {
+export function generate (contentDirectory, outputDirectory) {
   const actions = new FileSystemActions()
+
+  cleanUpDirectory(outputDirectory, actions)
+
   return actions
+}
+
+function cleanUpDirectory (directory, actions) {
+  if (fs.statSync(directory).isDirectory()) {
+    fs.readdirSync(directory).
+      /* Filter out .git and the likes. */
+      filter((path) => path.match(/^[^\.]/)).
+      forEach((path) => {
+        const fullPath = `${directory}/${path}`
+        if (fs.statSync(fullPath).isDirectory()) {
+          actions.add(new RemoveDirectoryAction(fullPath))
+        } else {
+          actions.add(new RemoveFileAction(fullPath))
+        }
+      })
+  } else {
+    actions.add(new CreateDirectoryAction(directory))
+  }
 }
