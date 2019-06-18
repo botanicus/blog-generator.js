22b0db34a3e444e9a75d3fe562eabc068b08f209
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2019-06-18 01:09:47 +0000

    Fix paths

--- post/router.mjs
+++ post/router.mjs
@@ -1,12 +1,18 @@
 import path from 'path'
 import { ensure } from '../utils.mjs'
 
-class Location {
-  constructor(timestamp, slug, path, defaultBasename) {
+/*
+  We need to be able to retrieve a post based on only its slug.
+  That means no timestamps in the output directory.
+
+  Timestamps are useful for organizing the content directory.
+*/
+
+class SourceLocation {
+  constructor(timestamp, slug, path) {
     this.timestamp = ensure(timestamp, `${this.constructor.name}: timestamp is required`)
     this.slug = ensure(slug, `${this.constructor.name}: slug is required`)
     this.path = ensure(path, `${this.constructor.name}: path is required`)
-    this.defaultBasename = ensure(defaultBasename, `${this.constructor.name}: defaultBasename is required`)
   }
 
   getDirectoryBaseName() {
@@ -17,7 +23,26 @@ class Location {
     return path.join(this.path, this.getDirectoryBaseName())
   }
 
-  getFilePath(basename = this.defaultBasename) {
+  getFilePath(basename = `${this.slug}.md`) {
+    return path.join(this.getDirectoryPath(), basename)
+  }
+}
+
+/*
+  output/hello-world/hello-world.json
+  output/hello-world/cute-kitty.png
+*/
+class OutputLocation {
+  constructor(slug, path) {
+    this.slug = ensure(slug, `${this.constructor.name}: slug is required`)
+    this.path = ensure(path, `${this.constructor.name}: path is required`)
+  }
+
+  getDirectoryPath() {
+    return path.join(this.path, this.slug)
+  }
+
+  getFilePath(basename = `${this.slug}.json`) {
     return path.join(this.getDirectoryPath(), basename)
   }
 }
@@ -30,9 +55,9 @@ export default class PathRouter {
     ensure(contentDirectory, `${this.constructor.name}: contentDirectory is required`)
     ensure(outputDirectory, `${this.constructor.name}: outputDirectory is required`)
 
-    this.originalSource = new Location(originalFileTimestamp, slug, contentDirectory, `${slug}.md`)
-    this.standardizedSource = new Location(publishedDateTimestamp, slug, contentDirectory, `${slug}.md`)
-    this.output = new Location(publishedDateTimestamp, slug, outputDirectory, `${slug}.json`)
+    this.originalSource = new SourceLocation(originalFileTimestamp, slug, contentDirectory)
+    this.standardizedSource = new SourceLocation(publishedDateTimestamp, slug, contentDirectory)
+    this.output = new OutputLocation(slug, outputDirectory)
   }
 
   hasSourceDirectoryChanged() {
